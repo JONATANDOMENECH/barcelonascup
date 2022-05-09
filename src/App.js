b81b74ccf209b3c451c36c6452eaa56790d174bd
JONATAN DOMENECH <jonatandomenech@yahoo.es>
2022-05-09 17:54:30 +0200

    Video

--- src/App.js
+++ src/App.js
@@ -7,16 +7,22 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        The Official website of Barcelona's Cup. 
-The competition for the newest trophy in international pollution
+        The Official website of Barcelona's Cup. The competition for the newest trophy in international pollution
         </p>
+        <iframe
+        width="620" height="350" src="https://www.youtube.com/embed/I3DrIJv4r60"
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+        title="video"
+      />{" "}
         <a
           className="App-link"
           href="https://americascup.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          America's Cup
+          Barcelona 37th America's Cup
         </a>
       </header>
     </div>
